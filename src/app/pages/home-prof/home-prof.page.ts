4415d33d2a6a6ad62bc8dafc48629ac15f289935
jacksontrr <57967781+jacksontrr@users.noreply.github.com>
2021-03-02 20:48:27 -0300

    Botão sair funcionando

--- src/app/pages/home-prof/home-prof.page.ts
+++ src/app/pages/home-prof/home-prof.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthcService } from 'src/app/services/authc.service';
+import { Platform, NavController, LoadingController, ToastController } from '@ionic/angular';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
+
 
 @Component({
   selector: 'app-home-prof',
@@ -7,9 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeProfPage implements OnInit {
 
-  constructor() { }
+  constructor(private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
+    private afAuth: AngularFireAuth,
+    private navCtrl: NavController,
+    private firestore: AngularFirestore,
+    private platform: Platform,
+    private autheService: AuthcService) { }
 
   ngOnInit() {
+  }
+
+  async logout() {
+    let aguarde = await this.loadingCtrl.create({
+      message: 'Aguarde por favor...',
+      duration: 900,
+    });
+
+    try {
+      await this.autheService.logout();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      aguarde.dismiss();
+    }
   }
 
 }
